62bd62d6b067a4e47d7410f5e38bf2551cb810a3
Arthur Etmekdjian <arthure@Arthurs-MacBook-Air.local>
2021-04-29 16:58:36 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles.css";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <h1>NBA Roster Project</h1>
+        <h2>Read instructions in README.md</h2>
+      </div>
+    </Provider>
   );
 }
-
-export default App;
